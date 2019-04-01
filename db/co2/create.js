a173245d95f0f74e478dca604921fa116a571dfd
egurinko <egctoru@gmail.com>
2019-04-01 12:36:42 +0900

    Changed to insert

--- db/co2/create.js
+++ db/co2/create.js
@@ -16,6 +16,15 @@ module.exports = client => {
 
 const insertDocuments = (db, data, callback) => {
   const collection = db.collection("co2");
+  // Dateオブジェクトを作成
+  let date = new Date();
+
+  // UNIXタイムスタンプを取得する (ミリ秒単位)
+  let a = date.getTime();
+
+  // UNIXタイムスタンプを取得する (秒単位 - PHPのtime()と同じ)
+  let b = Math.floor(a / 1000);
+  data.timestamp = b;
 
   collection.insertOne(data, (err, result) => {
     console.log("Inserted DATA", data);
