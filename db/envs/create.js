557cfbda432d4c6f8a58cc9fbdafd8613582e463
egurinko <egctoru@gmail.com>
2019-04-01 12:52:22 +0900

    Changed to insert

--- db/envs/create.js
+++ db/envs/create.js
@@ -1,34 +1,24 @@
 require("dotenv").config();
 const dbName = process.env.DB_NAME;
 
+const assert = require("assert");
+
 module.exports = client => {
-  return data => {
-    client.connect(err => {
+  return async data => {
+    try {
+      await client.connect();
       console.log("Connected successfully to server");
       const db = client.db(dbName);
 
-      insertDocuments(db, data, () => {
-        client.close();
-      });
-    });
+      let r = await db.collection("envs").insertOne(data);
+      assert.equal(1, r.insertedCount);
+
+      console.log("Inserted DATA", data);
+      console.log("RESULT", r);
+
+      client.close();
+    } catch (err) {
+      console.log(err.stack);
+    }
   };
-};
-
-const insertDocuments = (db, data, callback) => {
-  const collection = db.collection("envs");
-  // Dateオブジェクトを作成
-  let date = new Date();
-
-  // UNIXタイムスタンプを取得する (ミリ秒単位)
-  let a = date.getTime();
-
-  // UNIXタイムスタンプを取得する (秒単位 - PHPのtime()と同じ)
-  let b = Math.floor(a / 1000);
-  data.timestamp = b;
-
-  collection.insertOne(data, (err, result) => {
-    console.log("Inserted DATA", data);
-    console.log("RESULT", result);
-    callback(result);
-  });
 };
