2e7a8590f0087a0896cec4f448b49153ea0e80af
egurinko <egctoru@gmail.com>
2019-04-01 09:46:59 +0900

    Inserted req.body

--- server/api/envs/index.js
+++ server/api/envs/index.js
@@ -2,17 +2,15 @@ const router = require("express").Router();
 
 module.exports = services => {
   router.get("", (req, res) => {
-    console.log("Hello World");
     res.sendStatus(200);
   });
 
   router.post("", (req, res) => {
-    const rowData = req.body;
     let data;
-    for (let i in rowData) {
+    for (let i in req.body) {
       data = i;
     }
-    console.log("DATA", typeof data, JSON.parse(data));
+    console.log("DATA", typeof data, data);
     services.envs.create(data);
     res.sendStatus(200);
   });
