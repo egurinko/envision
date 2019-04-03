4d7f0ed368fe8d7c6b3d82eee7da58cf306062bb
egurinko <egctoru@gmail.com>
2019-04-03 21:19:01 +0900

    Changed the methods of sending api result

--- server/api/envs/index.js
+++ server/api/envs/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 module.exports = services => {
   router.get("/", async (req, res) => {
     const data = await services.envs.list();
-    res.status(200).send(data.data);
+    res.status(200).send(data);
   });
 
   router.post("/", (req, res) => {
