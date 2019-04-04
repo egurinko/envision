94b890fd981bd03b7cdec31b54f177d0a460bfa4
egurinko <egctoru@gmail.com>
2019-04-04 10:20:43 +0900

    Added CO2 and temperature chart

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,17 @@
 <template>
   <v-layout justify-center class="primary">
-    <v-flex xs12 sm6>
+    <v-flex xs12 sm6 v-if="loaded">
       <v-card class="secondary">
         <line-chart :chart-data="humidityChartData"></line-chart>
       </v-card>
       <v-card class="secondary">
         <line-chart :chart-data="pressureChartData"></line-chart>
+      </v-card>
+      <v-card class="secondary">
+        <line-chart :chart-data="tempChartData"></line-chart>
+      </v-card>
+      <v-card class="secondary">
+        <line-chart :chart-data="co2ChartData"></line-chart>
       </v-card>
     </v-flex>
   </v-layout>
@@ -22,6 +28,7 @@ export default {
   },
   data() {
     return {
+      loaded: false,
       envs: {
         humidity: [],
         pressure: [],
@@ -32,10 +39,33 @@ export default {
   },
   computed: {
     humidityChartData: function() {
+      if (!this.loaded) return;
       return this.makeChartData("humidity");
     },
     pressureChartData: function() {
+      if (!this.loaded) return;
       return this.makeChartData("pressure");
+    },
+    tempChartData: function() {
+      if (!this.loaded) return;
+      return this.makeChartData("temperature");
+    },
+    co2ChartData: function() {
+      if (!this.loaded) return;
+      const labels = this.co2.map(data => data.timestamp);
+      const data = this.co2.map(data => data.co2.value);
+      return {
+        labels,
+        datasets: [
+          {
+            label: "CO2",
+            backgroundColor: this.$store.state.colors.primary,
+            borderColor: this.$store.state.colors.lightGreen,
+            radius: 0,
+            data
+          }
+        ]
+      };
     }
   },
   created() {
@@ -62,6 +92,8 @@ export default {
           });
         });
         this.co2 = co2.data;
+        this.loaded = !this.loaded;
+
         console.debug(this.envs, this.co2);
       });
     },
