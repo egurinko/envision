9f5e81663657bb1678ee0298ab71cb1067f08597
egurinko <egctoru@gmail.com>
2019-04-14 01:40:24 +0900

    Added current comfort index to doughnut chart

--- src/module/doughnutChart.js
+++ src/module/doughnutChart.js
@@ -1,7 +1,9 @@
 import { Doughnut, mixins } from "vue-chartjs";
 const { reactiveProp } = mixins;
+import store from "../store.js";
 
 const fontColor = "white";
+let fontSize;
 
 const options = {
   scales: {},
@@ -15,7 +17,24 @@ const options = {
     fontColor,
     padding: 15
   },
-  tooltips: { enabled: true }
+  tooltips: { enabled: true },
+  animation: {
+    duration: 500,
+    onComplete: function() {
+      let chartContext = this.chart.canvas.getContext("2d", { alpha: false });
+      chartContext.fillStyle = fontColor;
+      chartContext.textAlign = "center";
+      chartContext.textBaseline = "middle";
+      fontSize = store.state.isPhone ? 30 : 40;
+      chartContext.font = `${fontSize}px Rubik`;
+
+      chartContext.fillText(
+        Math.round(this.data.datasets[0].data[0] * 100) + "%",
+        this.chart.width / 2,
+        this.chart.height / 2 + fontSize / 1.6
+      );
+    }
+  }
 };
 
 export default {
