2183bd6a7bb55d34f25efd58f4ff693573ea4f19
egurinko <egctoru@gmail.com>
2019-04-17 18:40:03 +0900

    Changed envs list api to extract all environment data

--- db/envs/list.js
+++ db/envs/list.js
@@ -2,7 +2,21 @@ const callDB = require("../module/callDB");
 
 module.exports = () => {
   return async () => {
-    const data = await callDB("GET", "envs");
-    return data;
+    const co2 = await callDB("GET", "co2");
+    const colorTemp = await callDB("GET", "color_temp");
+    const humidity = await callDB("GET", "hum");
+    const lux = await callDB("GET", "lux");
+    const pressure = await callDB("GET", "pressure");
+    const temperature = await callDB("GET", "temp");
+    const snack = await callDB("GET", "weight");
+    return [
+      { key: "co2", data: co2 },
+      { key: "color temperature", data: colorTemp },
+      { key: "humidity", data: humidity },
+      { key: "lux", data: lux },
+      { key: "pressure", data: pressure },
+      { key: "temperature", data: temperature },
+      { key: "snack", data: snack }
+    ];
   };
 };
