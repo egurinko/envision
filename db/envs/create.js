8583b94858feb969564e7ce1ab7337e19bc0b864
egurinko <egctoru@gmail.com>
2019-04-17 17:52:27 +0900

    Accept array from envs data

--- db/envs/create.js
+++ db/envs/create.js
@@ -2,6 +2,8 @@ const callDB = require("../module/callDB");
 
 module.exports = () => {
   return async data => {
-    await callDB("POST", "envs", data);
+    for (let obj of data) {
+      await callDB("POST", obj.type, { value: obj.value });
+    }
   };
 };
