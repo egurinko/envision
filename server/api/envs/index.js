2fdd7cfb5d28a7fb7b81481be840eeabdba4d2db
egurinko <egctoru@gmail.com>
2019-04-19 22:37:51 +0900

    Added button to change the timespan

--- server/api/envs/index.js
+++ server/api/envs/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 module.exports = services => {
   router.get("/", async (req, res) => {
-    console.log("TIMESPAN BACKEND: ", req.params.timespan);
+    console.log("TIMESPAN BACKEND: ", req.query.timespan);
     const data = await services.envs.list(req.params.timespan);
     res.status(200).send(data);
   });
