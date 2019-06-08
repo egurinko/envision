105f8086204bf7f067b887ccf3451384a65c1e2c
egurinko <egctoru@gmail.com>
2019-06-09 01:09:01 +0900

    Move Annotations from vuex store to constant

--- src/store.js
+++ src/store.js
@@ -14,31 +14,8 @@ export default new Vuex.Store({
         ? "https://prod-bot-staging.herokuapp.com/api"
         : "/api",
     colors,
-    auth: {
-      username: "",
-      token: ""
-    },
-    annotations: {
-      TEMPERATURE: {
-        "LOWER LIMIT": constant.TEMP_LOWER_LIMIT,
-        "UPPER LIMIT": constant.TEMP_UPPER_LIMIT
-      },
-      HUMIDITY: {
-        "LOWER LIMIT": constant.HUM_LOWER_LIMIT,
-        "UPPER LIMIT": constant.HUM_UPPER_LIMIT
-      },
-      CO2: { "UPPER LIMIT": constant.CO2_UPPER_LIMIT },
-      LUX: {
-        "LOWER LIMIT": constant.LUX_LOWER_LIMIT,
-        "UPPER LIMIT": constant.LUX_UPPER_LIMIT
-      },
-      PRESSURE: { "LOWER LIMIT": constant.PRESSURE_LOWER_LIMIT },
-      "COLOR TEMPERATURE": {
-        "LOWER LIMIT": constant.COLOR_TEMP_LOWER_LIMIT,
-        "UPPER LIMIT": constant.COLOR_TEMP_UPPER_LIMIT
-      },
-      SNACK: { "LOWER LIMIT": constant.SNACK_LOWER_LIMIT }
-    },
+    username: "",
+    annotations: constant.ANNOTATIONS,
     timespans: {
       selected: "AN HOUR",
       timespan: {
@@ -59,8 +36,8 @@ export default new Vuex.Store({
     changeTimespan(state, timespan) {
       state.timespans.selected = timespan;
     },
-    setAuth(state, auth) {
-      state.auth = auth;
+    setUsername(state, username) {
+      state.username = username;
     }
   },
   actions: {},
