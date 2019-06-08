32f466a5e7b4721be964c2ec0f4508686573c48c
egurinko <egctoru@gmail.com>
2019-06-09 01:45:39 +0900

    Allow x-access-token to access to api

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ const allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
+    "Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token"
   );
 
   if ("OPTIONS" == req.method) {
