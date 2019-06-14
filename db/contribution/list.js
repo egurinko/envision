836f69788eeeb387e8d467ae8496ead5016ef8b0
egurinko <egctoru@gmail.com>
2019-06-15 00:23:28 +0900

    Added contribution GET API

--- db/contribution/list.js
+++ db/contribution/list.js
@@ -1,4 +1,27 @@
-const callDB = require("../module/callDB");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const domain = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
+const parser = { useNewUrlParser: true };
+
+const callDB = async (method, endpoint) => {
+  const client = MongoClient(domain, parser);
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    const data = await db
+      .collection(endpoint)
+      .find({})
+      .toArray();
+
+    client.close();
+
+    return data;
+  } catch (err) {
+    throw Error(err);
+  }
+};
 
 module.exports = () => {
   return async () => {
