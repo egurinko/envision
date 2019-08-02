a0111e1f2b17551a23ef7825559d4730171c4bdf
egurinko <egctoru@gmail.com>
2019-08-02 23:19:25 +0900

    added verify token to envs

--- server/routes/envs/index.js
+++ server/routes/envs/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const envsController = require("../../controller/envs/index");
+const verifyToken = require("../../middleware/verifyToken");
 
 module.exports = services => {
   router.get("/", async (req, res) => envsController.list(req, res, services));
 
-  router.post("/", async (req, res) =>
+  router.post("/", verifyToken, async (req, res) =>
     envsController.create(req, res, services)
   );
 
