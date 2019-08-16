628d521ab39cdc71df754c5630186ba38c34b61c
egurinko <egctoru@gmail.com>
2019-08-17 02:12:14 +0900

    Added tssupport

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: ["plugin:vue/recommended"],
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
@@ -14,7 +17,13 @@ module.exports = {
     semi: ["error", "always"],
     quotes: ["error", "double"],
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: '@typescript-eslint/parser'
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/typescript'
+  ]
 };
