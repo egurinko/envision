b8f0df0105817923213800192d147c393700fb43
egurinko <egctoru@gmail.com>
2019-08-19 00:29:17 +0900

    Added login test

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,21 @@
+describe("Login", () => {
+    const username = Cypress.env("username");
+    const password = Cypress.env("password");
+  
+    beforeEach(() => {
+      cy.visit("/login");
+  
+      cy.server();
+      cy.route("POST", "/api/auth/login").as("login");
+    });
+  
+    it("should be done with valid user", () => {
+      cy.get("[data-cy=username]").type(username);
+      cy.get("[data-cy=password]").type(password);
+      cy.get("[data-cy=login-button]").click();
+  
+      cy.wait("@login").then(res =>{
+        expect(res.status).eq(200);
+      });
+    });
+  });
