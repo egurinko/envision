e81262b152cd4b118618f977c1219baf7ddb3df6
egurinko <egctoru@gmail.com>
2019-08-19 00:09:35 +0900

    ddd

--- db/seed/user/index.js
+++ db/seed/user/index.js
@@ -8,7 +8,7 @@ module.exports = [
     },
     {
         "username" : "cypress",
-        "password": "$2a$08$HQK3lLpIrED6QrAK5SvWFu9RKlcfWkv.r4F6q/h6BsWoxDtwOrm8W",
+        "password": "$2a$08$OU/cTyjU/PSR.ZIiZRW3B.z4cx6181r2/xiyPiG2sFz94sPClqzdO",
         "timestamp" : date
     }
 ];
