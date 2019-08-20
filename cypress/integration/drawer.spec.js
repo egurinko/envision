723fbb810794420c889cb86201774b128dffbec7
egurinko <egctoru@gmail.com>
2019-08-20 15:54:50 +0900

    Added routing test

--- cypress/integration/drawer.spec.js
+++ cypress/integration/drawer.spec.js
@@ -16,4 +16,15 @@ describe("Drawer", () => {
     expect(cy.get("[data-cy=bar_chart]")).to.exist;
     expect(cy.get("[data-cy=supervisor_account]")).to.exist;
   });
+
+  it("icons should enable users to go target routes", () => {
+    cy.get("[data-cy=bar_chart]").click();
+    cy.url().should("include", "/graphs");
+
+    cy.get("[data-cy=supervisor_account]").click();
+    cy.url().should("include", "/users");
+
+    cy.get("[data-cy=home]").click();
+    cy.url().should("include", "/");
+  });
 });
